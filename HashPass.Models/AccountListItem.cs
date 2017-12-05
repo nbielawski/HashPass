7bdb85a35ec860d35e19fa7125b81fe34b6d7cd9
Nick Bielawski <nbielawski12@gmail.com>
2017-12-05 10:31:22 -0500

    Added Password conroller and View

---
+++ HashPass.Models/AccountListItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPass.Models
+{
+    public class AccountListItem
+    {
+        public int AccountId { get; set; }
+
+        public string  AcctName { get; set; }
+
+        public string AcctPassword { get; set; }
+
+
+        [Display(Name="Added")]
+        public DateTimeOffset AddedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ HashPass.Web/Controllers/PasswordController.cs
@@ -0,0 +1,20 @@
+﻿using HashPass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HashPass.Web.Controllers
+{
+    public class PasswordController : Controller
+    {
+        // GET: Password
+        [Authorize]
+        public ActionResult Index()
+        {
+            var model = new AccountListItem [0];
+            return View(model);
+        }
+    }
+}
